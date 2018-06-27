191c37e1a30d3445bde62408752808c201e4f5f0
eratostennis <eiji.sekiya.0326@gmail.com>
2018-06-27 06:13:53 +0000

    Change lib file path

--- python/darknet_pywrapper.py
+++ python/darknet_pywrapper.py
@@ -46,7 +46,7 @@ class METADATA(Structure):
     
 
 #lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
-lib = CDLL("libdarknet.so", RTLD_GLOBAL)
+lib = CDLL("/home/automanuser/automan/worker/yolov3/darknet/libdarknet.so", RTLD_GLOBAL)
 
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
